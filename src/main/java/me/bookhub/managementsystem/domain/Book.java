e02a11f77c91eab9d2451470654e500717a2e624
kimsh1691 <kimsh1691@gmail.com>
2024-06-14 16:35:08 +0900

    domain 생성

---
+++ src/main/java/me/bookhub/managementsystem/domain/Book.java
@@ -0,0 +1,24 @@
+package me.bookhub.managementsystem.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String author;
+    private String isbn;
+    private String publisher;
+    private String publishDate;
+
+}

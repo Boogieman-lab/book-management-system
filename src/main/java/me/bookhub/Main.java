04ce8b99e570d802b6edb526c8d355b662f9fc2c
seungwon <swlee173@gmail.com>
2024-06-15 16:55:05 +0900

    테스트

--- src/main/java/me/bookhub/Main.java
+++ src/main/java/me/bookhub/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("테스트");
         SpringApplication.run(Main.class, args);
     }
 }

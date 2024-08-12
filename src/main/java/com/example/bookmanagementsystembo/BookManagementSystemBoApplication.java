4c33a1ad651683b10ec653287b213d1c3a87eb50
seungwon <swlee173@gmail.com>
2024-08-12 17:39:32 +0900

    init

---
+++ src/main/java/com/example/bookmanagementsystembo/BookManagementSystemBoApplication.java
@@ -0,0 +1,13 @@
+package com.example.bookmanagementsystembo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookManagementSystemBoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookManagementSystemBoApplication.class, args);
+    }
+
+}

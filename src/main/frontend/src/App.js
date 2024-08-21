9bdeea092f08129d422668926ed9f69d27c0298e
seungwon <swlee173@gmail.com>
2024-08-22 00:24:33 +0900

    해더에 AppBar 구현 및 스타일 커스텀 작업

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,20 +1,29 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/Common/Sidebar";
+import Header from "./components/Common/Header";
+import { Box, CssBaseline } from '@mui/material';
 
 function App() {
     return (
         <Router>
-            <div className="App">
-                <Sidebar>
+            <Box sx={{ display: 'flex' }}>
+                <CssBaseline />
+
+                {/* 좌측 고정 사이드바 */}
+                <Sidebar />
+
+                {/* 상단 고정 헤더 */}
+                <Header />
+
+                {/* 메인 콘텐츠 영역 */}
+                <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8 }}>
                     <Routes>
                     </Routes>
-                </Sidebar>
-            </div>
+                </Box>
+            </Box>
         </Router>
     );
 }
-
 
 export default App;

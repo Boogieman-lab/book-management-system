e1d672e87296931a5be4a9e1995a427ba35a6372
seungwon <swlee173@gmail.com>
2024-08-22 00:59:16 +0900

    코드 정리

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import {BrowserRouter as Router, Routes} from 'react-router-dom';
 import Sidebar from "./components/Common/Sidebar";
 import Header from "./components/Common/Header";
 import {Box, CssBaseline} from '@mui/material';
@@ -8,10 +8,12 @@ function App() {
     return (
         <Router>
             <Box sx={{display: 'flex'}}>
-                <CssBaseline/> {/* 브라우저 기본 스타일을 초기화 */}
-
-                <Sidebar/> {/* 좌측 고정 사이드바 */}
-                <Header/> {/* 상단 고정 헤더 */}
+                {/* 브라우저 기본 스타일을 초기화 */}
+                <CssBaseline/>
+                {/* 좌측 고정 사이드바 */}
+                <Sidebar/>
+                {/* 상단 고정 헤더 */}
+                <Header/>
 
                 {/* 메인 콘텐츠 영역 */}
                 <Box component="main" sx={{flexGrow: 1, p: 3, mt: 8}}>

5e145ec6f18f83b43b11226588dbe16e9ee05a7b
seungwon <swlee173@gmail.com>
2024-08-22 00:28:33 +0900

    코드 정리 및 주석 처리

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Sidebar from "./components/Common/Sidebar";
 import Header from "./components/Common/Header";
-import { Box, CssBaseline } from '@mui/material';
+import {Box, CssBaseline} from '@mui/material';
 
 function App() {
     return (
         <Router>
-            <Box sx={{ display: 'flex' }}>
-                <CssBaseline />
+            <Box sx={{display: 'flex'}}>
+                <CssBaseline/> {/* 브라우저 기본 스타일을 초기화 */}
 
-                {/* 좌측 고정 사이드바 */}
-                <Sidebar />
-
-                {/* 상단 고정 헤더 */}
-                <Header />
+                <Sidebar/> {/* 좌측 고정 사이드바 */}
+                <Header/> {/* 상단 고정 헤더 */}
 
                 {/* 메인 콘텐츠 영역 */}
-                <Box component="main" sx={{ flexGrow: 1, p: 3, mt: 8 }}>
+                <Box component="main" sx={{flexGrow: 1, p: 3, mt: 8}}>
                     <Routes>
+                        {/* 라우트 정의는 여기에 추가 */}
                     </Routes>
                 </Box>
             </Box>

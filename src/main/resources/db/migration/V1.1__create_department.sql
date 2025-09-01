107733b4a6101727e709e0036f7aa1382e957cdf
seungwonlee <swlee173@gmail.com>
2025-09-01 10:19:40 +0900

    feat: flyway, jpa 설정 및 mariadb 접속 정보 추가

---
+++ src/main/resources/db/migration/V1.1__create_department.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    department_id    BIGINT AUTO_INCREMENT COMMENT '부서 ID',
+    name             VARCHAR(50) COMMENT '사용자 ID',
+    created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+    updated_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+    PRIMARY KEY (department_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='사용자 정보 테이블';

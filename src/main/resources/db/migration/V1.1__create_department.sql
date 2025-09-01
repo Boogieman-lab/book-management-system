9b579f966070578f208c23af7bfdf2fd7d1de8c4
seungwonlee <swlee173@gmail.com>
2025-09-01 10:47:51 +0900

    feat: flyway DDL 작성

--- src/main/resources/db/migration/V1.1__create_department.sql
+++ src/main/resources/db/migration/V1.1__create_department.sql
@@ -1,8 +1,8 @@
-CREATE TABLE users
+CREATE TABLE department
 (
     department_id    BIGINT AUTO_INCREMENT COMMENT '부서 ID',
     name             VARCHAR(50) COMMENT '사용자 ID',
     created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
     updated_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
     PRIMARY KEY (department_id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='사용자 정보 테이블';
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='부서 테이블';

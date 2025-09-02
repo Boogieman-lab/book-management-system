42f964b77d617250646d37e647be05300bc35624
seungwonlee <swlee173@gmail.com>
2025-09-02 19:02:17 +0900

    feat: 초기 마이그레이션 추가 및 ERD 반영

---
+++ src/main/resources/db/migration/V1.8__create_book_borrow.sql
@@ -0,0 +1,12 @@
+CREATE TABLE book_borrow
+(
+    book_borrow_id   BIGINT AUTO_INCREMENT COMMENT '대출 ID',
+    book_hold_id     BIGINT NOT NULL COMMENT '도서 보유 ID',
+    book_id          BIGINT NOT NULL COMMENT '도서 ID',
+    user_id          BIGINT NOT NULL COMMENT '사용자 ID',
+    reason           VARCHAR(512) COMMENT '대출 사유',
+    status           VARCHAR(50) NOT NULL COMMENT '대출 상태',
+    created_at       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
+    updated_at       DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+    PRIMARY KEY (book_borrow_id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='도서 대출 테이블';

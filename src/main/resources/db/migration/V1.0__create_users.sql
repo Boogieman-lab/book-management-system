4517110f86d2ac4cf91f8a5d415d92a747d38364
seungwonlee <swlee173@gmail.com>
2025-09-22 17:21:50 +0900

    fix: updatedAt 기본값 null 및 insertable 으로 수정

--- src/main/resources/db/migration/V1.0__create_users.sql
+++ src/main/resources/db/migration/V1.0__create_users.sql
@@ -9,6 +9,6 @@ CREATE TABLE users
     role             VARCHAR(20)  NOT NULL COMMENT '권한',
     login_fail_count INT          NOT NULL DEFAULT 0 COMMENT '로그인 실패 횟수',
     created_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '생성일시',
-    updated_at       DATETIME     NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
+    updated_at       DATETIME NULL DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP COMMENT '수정일시',
     PRIMARY KEY (user_id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci COMMENT='사용자 테이블';

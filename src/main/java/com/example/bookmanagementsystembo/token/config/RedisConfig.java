69b3de7f193781b6bb72e8c0a4c1413dd78e5971
seungwonlee <swlee173@gmail.com>
2025-10-28 18:16:39 +0900

    chore: redis 키스페이스 이벤트 리스너 활성화 설정 추가

--- src/main/java/com/example/bookmanagementsystembo/token/config/RedisConfig.java
+++ src/main/java/com/example/bookmanagementsystembo/token/config/RedisConfig.java
@@ -3,9 +3,13 @@ package com.example.bookmanagementsystembo.token.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisKeyValueAdapter;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
+@EnableRedisRepositories(enableKeyspaceEvents
+        = RedisKeyValueAdapter.EnableKeyspaceEvents.ON_STARTUP)
 @Configuration
 public class RedisConfig {
     @Bean

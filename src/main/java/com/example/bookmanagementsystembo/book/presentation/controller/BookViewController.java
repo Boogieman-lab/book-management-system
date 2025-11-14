a1f54b51dfd8645d04908283eeaf72966f5db3d0
kim-soohyeon <kimsh1691@gmail.com>
2025-11-14 16:12:39 +0900

    feat: 사이드바 활성 메뉴 표시를 위해 activeMenu 모델 속성 추가

--- src/main/java/com/example/bookmanagementsystembo/book/presentation/controller/BookViewController.java
+++ src/main/java/com/example/bookmanagementsystembo/book/presentation/controller/BookViewController.java
@@ -5,14 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/user/book")
 public class BookViewController {
 
     @GetMapping("/bookList")
-    public String bookList() {
+    public String bookList(Model model) {
+        model.addAttribute("activeMenu", "books");
         return "user/book/book_list"; // 경로 수정
     }
 
